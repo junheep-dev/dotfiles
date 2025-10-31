e7cd9f49955e5422c1c9ba1018d0d4d140aa785e
Junhee Park <12604726+junheep-dev@users.noreply.github.com>
2025-10-31 20:14:37 +0900

    Set thick border to code block in render-markdown.nvim

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        -- Use thick border for code blocks to prevent height shifting
+        -- when switching between normal and insert modes
+        border = "thick",
+      },
+    },
+  },
+}
