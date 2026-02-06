3ac115b7ca1182f962d3cfe047f469832898dbcc
Junhee Park <12604726+junheep-dev@users.noreply.github.com>
2026-02-06 09:23:44 +0900

    Remove dnd setup as karabiner supports it natively

--- scripts/utilities/karabiner.sh
+++ scripts/utilities/karabiner.sh
@@ -19,7 +19,6 @@ confirm_action "Have you completed granting permissions? Press 'y' and Enter to 
 print_step "Set keyboard shortcuts in System settings"
 echo "Please set the following keyboard shortcuts in System Settings > Keyboard > Keyboard Shortcuts:"
 echo "1. Input Sources > Select the previous input source: F18"
-echo "2. Mission Control > Turn Do Not Disturb On/Off: F16"
 confirm_action "Press 'y' and Enter when you have completed setting the shortcuts... "
 
 print_success "Karabiner setup complete"
