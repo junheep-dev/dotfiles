55d8457a27fd914fb9653c8f58dbe43567b62efb
Junhee Park <12604726+junheep-dev@users.noreply.github.com>
2026-03-11 10:39:44 +0900

    Add diffview.nvim plugin with keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -34,4 +34,32 @@ return {
       },
     },
   },
+
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Diffview open" },
+      {
+        "<leader>gdc",
+        function()
+          vim.ui.input({ prompt = "DiffviewOpen (e.g. main, HEAD~3, main..HEAD): " }, function(input)
+            if input and input ~= "" then
+              vim.cmd("DiffviewOpen " .. input)
+            end
+          end)
+        end,
+        desc = "Diffview open with args",
+      },
+      { "<leader>gdh", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (current file)" },
+      { "<leader>gdH", "<cmd>DiffviewFileHistory<cr>", desc = "File history (all)" },
+    },
+    opts = {
+      keymaps = {
+        view = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
+        file_panel = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
+        file_history_panel = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
+      },
+    },
+  },
 }
