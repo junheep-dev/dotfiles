653c3438ae6789a3495f1637f7202a8be57a131f
Junhee Park <12604726+junheep-dev@users.noreply.github.com>
2026-06-25 16:36:32 +0900

    Set up tokyonight colorscheme

--- nvim-custom/lua/plugins/colorscheme.lua
+++ nvim-custom/lua/plugins/colorscheme.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- main theme: load on startup, not lazily
+    priority = 1000, -- load before other plugins so highlights aren't clobbered
+    opts = { style = "moon" },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+}
