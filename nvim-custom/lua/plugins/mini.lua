b83c1a11ffd8eb59b3929d27c2b212cdff547cf0
Junhee Park <12604726+junheep-dev@users.noreply.github.com>
2026-07-08 12:51:14 +0900

    Add mini.jump/jump2d, mini.files, mini.hipatterns, mini.statusline
    Chose mini.jump+jump2d over flash.nvim (flash's search-integration,
    treesitter-node-jump, and remote-actions extras weren't things we'd
    actually use), mini.files over neo-tree/Snacks.explorer, mini.statusline
    over lualine (falls back to gitsigns for git info, matching our
    earlier gitsigns choice), and mini.hipatterns scoped to just hex-color
    highlighting. trouble.nvim and todo-comments.nvim deliberately deferred.

--- nvim-custom/lua/plugins/mini.lua
+++ nvim-custom/lua/plugins/mini.lua
@@ -25,6 +25,67 @@ return {
     opts = {},
   },
   {
+    "nvim-mini/mini.pick",
+    version = "*",
+    opts = {},
+    keys = {
+      { "<leader>ff", "<Cmd>Pick files<CR>", desc = "Files" },
+      { "<leader>fg", "<Cmd>Pick grep_live<CR>", desc = "Grep Live" },
+      { "<leader>fG", '<Cmd>Pick grep pattern="<cword>"<CR>', desc = "Grep Current Word" },
+      { "<leader>fb", "<Cmd>Pick buffers<CR>", desc = "Buffers" },
+      { "<leader>fh", "<Cmd>Pick help<CR>", desc = "Help Tags" },
+      { "<leader>fr", "<Cmd>Pick resume<CR>", desc = "Resume" },
+    },
+  },
+  {
+    "nvim-mini/mini.extra",
+    version = "*",
+    opts = {},
+    keys = {
+      { "<leader>fd", '<Cmd>Pick diagnostic scope="all"<CR>', desc = "Diagnostic Workspace" },
+      { "<leader>fD", '<Cmd>Pick diagnostic scope="current"<CR>', desc = "Diagnostic Buffer" },
+      { "<leader>fs", '<Cmd>Pick lsp scope="workspace_symbol_live"<CR>', desc = "Symbols Workspace" },
+      { "<leader>fS", '<Cmd>Pick lsp scope="document_symbol"<CR>', desc = "Symbols Document" },
+      { "<leader>fR", '<Cmd>Pick lsp scope="references"<CR>', desc = "References (LSP)" },
+    },
+  },
+  {
+    "nvim-mini/mini.jump",
+    version = "*",
+    opts = {},
+  },
+  {
+    "nvim-mini/mini.jump2d",
+    version = "*",
+    opts = {},
+  },
+  {
+    "nvim-mini/mini.files",
+    version = "*",
+    opts = {},
+    keys = {
+      { "<leader>ef", function() require("mini.files").open(vim.api.nvim_buf_get_name(0)) end, desc = "Explorer (current file)" },
+      { "<leader>ed", function() require("mini.files").open(vim.uv.cwd()) end, desc = "Explorer (cwd)" },
+    },
+  },
+  {
+    "nvim-mini/mini.hipatterns",
+    version = "*",
+    opts = function()
+      local hipatterns = require("mini.hipatterns")
+      return {
+        highlighters = {
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      }
+    end,
+  },
+  {
+    "nvim-mini/mini.statusline",
+    version = "*",
+    opts = {},
+  },
+  {
     "nvim-mini/mini.clue",
     version = "*",
     config = function()
@@ -42,6 +103,7 @@ return {
         clues = {
           { mode = "n", keys = "<leader>b", desc = "+buffer" },
           { mode = "n", keys = "<leader>c", desc = "+code" },
+          { mode = "n", keys = "<leader>e", desc = "+explore" },
           { mode = "n", keys = "<leader>f", desc = "+file" },
           { mode = "n", keys = "<leader>t", desc = "+tabs" },
           { mode = "n", keys = "<leader>u", desc = "+ui" },
