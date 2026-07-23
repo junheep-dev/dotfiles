9a03fc26f28e64e4fa6948d45e76d2da3df0b766
Junhee Park <12604726+junheep-dev@users.noreply.github.com>
2026-07-23 11:50:25 +0900

    Remove permission set up for alt tab and hammerspoon.

--- scripts/utilities/alt-tab.sh
+++ scripts/utilities/alt-tab.sh
@@ -13,10 +13,4 @@ print_step "Apply preferences"
 killall AltTab 2>/dev/null
 defaults import com.lwouis.alt-tab-macos "$DOTFILES_DIR/scripts/utilities/alt-tab.plist"
 
-print_step "Allow security permissions"
-echo -n "Press Enter to open AltTab. Then grant the required permissions."
-read -r
-open -a "AltTab"
-confirm_action "Have you completed granting permissions? Press 'y' and Enter to continue... "
-
 print_success "AltTab setup complete"
--- scripts/utilities/hammerspoon.sh
+++ scripts/utilities/hammerspoon.sh
@@ -10,10 +10,4 @@ mkdir -p "$HOME/.hammerspoon"
 rm -f $HOME/.hammerspoon/init.lua
 ln -sf "$DOTFILES_DIR/hammerspoon/init.lua" "$HOME/.hammerspoon/init.lua"
 
-print_step "Allow security permissions"
-echo -n "Press Enter to open Hammerspoon. Then grant the required permissions."
-read -r
-open -a "Hammerspoon"
-confirm_action "Have you completed granting permissions? Press 'y' and Enter to continue... "
-
 print_success "Hammerspoon setup complete"
